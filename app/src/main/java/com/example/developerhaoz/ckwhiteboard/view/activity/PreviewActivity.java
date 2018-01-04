e7ff577f7ace67a811cd2b5e4c155115d9a35d11
Haoz <developerhaon@gmail.com>
2018-01-04 20:26:36 +0800

    开始实现将图片写入到 U 盘中

--- app/src/main/java/com/example/developerhaoz/ckwhiteboard/view/activity/PreviewActivity.java
+++ app/src/main/java/com/example/developerhaoz/ckwhiteboard/view/activity/PreviewActivity.java
@@ -10,6 +10,7 @@ import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -58,6 +59,8 @@ public class PreviewActivity extends AppCompatActivity {
 
     private static final String KEY_IMAGE_URL = "imageUrl";
     private static final String KEY_EVALUATION = "evaluation";
+
+    private static final String TAG = "Preview111111";
 
     /**
      * 进行 5s 倒计时保存图片
@@ -137,6 +140,7 @@ public class PreviewActivity extends AppCompatActivity {
 
         String path = Environment.getExternalStorageDirectory().getAbsolutePath() + "/doodle/" + System.currentTimeMillis() + ".png";
         SavePictureUtil.savePicByPNG(bitmap, path);
+        Log.d(TAG, path);
         return path;
     }
 
